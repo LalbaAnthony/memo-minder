f246256ede6c56fc61211b48853c991adb4170f6
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-04-28 23:42:07 +0200

    Configure app

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,12 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
+      <nav class="bg-red">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
---
+++ front/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
