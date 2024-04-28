d1411f4b94b64efd636877a670ff9645addea2ef
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-04-29 01:01:35 +0200

    Damn apexChart finally work

--- front/src/main.js
+++ front/src/main.js
@@ -7,10 +7,13 @@ import { createPinia } from 'pinia'
 import './tailwind.css'
 import App from './App.vue'
 import router from './router'
+import VueApexCharts from 'vue3-apexcharts';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueApexCharts);
+app.component('apexChart', VueApexCharts)
 
 app.mount('#app')
