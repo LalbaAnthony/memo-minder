441e228ff5469750d540fbc535278caa0ddfe371
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-04-29 01:31:06 +0200

    Add icons and others

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: '#248f89',
+      'primary-light': '#4dcfc1',
+      light: '#ffffff',
+      gray: '#4B5563',
+      dark: '#090E16',
+      'light-dark': '#121826',
+      success: '#3caf57',
+      'light-success': '#b8ffc9',
+      warning: '#cf4a02',
+      'light-warning': '#fabd9c',
+      danger: '#cf0d0d',
+      'light-danger': '#fda8a8',
+    },
   },
   plugins: [],
 }
