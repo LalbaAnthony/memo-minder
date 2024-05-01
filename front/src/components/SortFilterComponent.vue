803e4f6978c6f464cf9d5e9f3d827f625d19f374
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-01 19:59:17 +0200

    The dropdown mission is a success

--- front/src/components/SortFilterComponent.vue
+++ front/src/components/SortFilterComponent.vue
@@ -1,38 +1,19 @@
 <template>
   <div class="flex justify-between items-center m-2 mb-6">
     <!-- Filter -->
-    <div>FILTER MENU HERE</div>
+    <Dropdown title="Filter" side="left">
+      hey
+    </Dropdown>
     <!-- Sort -->
-    <Menu as="div" class="relative inline-block text-left">
-      <div>
-        <MenuButton
-          class="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-light hover:bg-dark-gray"
-          @click.stop="showSort = !showSort">
-          Options
-          <ChevronDownIcon :class="['ml-2 h-5 w-5 text-light transform transition-transform', showSort ? 'rotate-180' : '']" aria-hidden="true" />
-        </MenuButton>
-      </div>
-      <TransitionRoot :show="showSort">
-        <!-- Hide at click outside -->
-        <TransitionChild v-click-outside="() => showSort = false" as="div"
-          enter="transition ease-out duration-100 transform" enter-from="opacity-0 scale-95"
-          enter-to="opacity-100 scale-100" leave="transition ease-in duration-75 transform"
-          leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95"
-          class="origin-top-right absolute right-0 mt-2 w-56 rounded-lg bg-gray p-4 z-20">
-          SORT<br>
-          SORT<br>
-          SORT<br>
-          SORT<br>
-        </TransitionChild>
-      </TransitionRoot>
-    </Menu>
+    <Dropdown title="Sort" side="right">
+      hey
+    </Dropdown>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { Menu, TransitionRoot, TransitionChild } from '@headlessui/vue'
-import { ChevronDownIcon } from '@heroicons/vue/24/solid'
+import Dropdown from '@/components/DropdownComponent.vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
