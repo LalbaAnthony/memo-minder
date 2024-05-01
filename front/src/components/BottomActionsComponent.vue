c66e9cbae712b7239924a8ad332a481407967698
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-01 22:51:03 +0200

    Push

---
+++ front/src/components/BottomActionsComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed bottom-0 right-0 z-30 flex flex-col justify-end gap-2 p-4">
+    <div v-if="isMobile()" @click="goToTop()" class="flex items-center justify-center bg-dark-gray text-white rounded-full p-4">
+      <ChevronUpIcon class="h-8 w-8" />
+    </div>
+    <router-link v-for="action in props.actions" :key="action.label" :to="action.to"
+    class="flex items-center justify-center bg-dark-gray text-white rounded-full px-4 py-2 mx-2">
+      {{ action.label }}
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import { ChevronUpIcon } from '@heroicons/vue/24/solid'
+import { isMobile } from '@/helpers/helpers.js'
+
+const props = defineProps({
+  actions: {
+    type: Array,
+    required: true,
+  },
+})
+
+function goToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+</script>
---
+++ front/src/components/SpanGraphComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <apexchart type="rangeBar" height="450" :options="chartOptions" :series="series"></apexchart>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const chartOptions = ref({
+  chart: {
+    height: 450,
+    type: 'rangeBar'
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      barHeight: '80%'
+    }
+  },
+  xaxis: {
+    type: 'datetime'
+  },
+  fill: {
+    type: 'solid',
+    opacity: 1
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'left'
+  }
+});
+
+const series = ref([
+  {
+    name: 'Mermoz',
+    data: [
+      {
+        x: 'Home',
+        y: [
+          new Date('2021-09-01').getTime(),
+          new Date('2022-06-01').getTime()
+        ]
+      },
+    ]
+  },
+  {
+    name: 'Bagatelle',
+    data: [
+      {
+        x: 'Home',
+        y: [
+          new Date('2022-06-01').getTime(),
+          new Date().getTime()
+        ]
+      },
+    ]
+  },
+]);
+
+</script>
+
+<style scoped>
+.apexcharts-text {
+  color: var(--light) !important;
+}
+</style>
