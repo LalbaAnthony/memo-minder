5554494ad6d9d5838b863e6755fe21c25bcd38e1
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-03 00:14:17 +0200

    LOT OF CHANGES

--- front/src/components/HeaderComponent.vue
+++ front/src/components/HeaderComponent.vue
@@ -5,7 +5,7 @@
       <Bars3Icon class="size-10 text-light-gray hover:bg-dark-gray rounded-lg p-1 cursor-pointery" @click.stop="showSidebar = !showSidebar" />
       <router-link to="/">
         <h1 class="text-2xl font-bold text-light">
-          {{ SITE_NAME }}
+          {{ route.meta.title }}
         </h1>
       </router-link>
       <MagnifyingGlassIcon class="size-10 text-light-gray hover:bg-dark-gray rounded-full p-1 cursor-pointer" @click.stop="focusSearchBar" />
@@ -40,15 +40,20 @@
             <span class="ml-3 mt-0.5">Home</span>
           </router-link>
           <hr>
-          <router-link to="/time-spans" class="flex items-center text-light rounded-lg 
+          <router-link to="/seasons" class="flex items-center text-light rounded-lg 
           hover:bg-dark-gray p-2" @click.stop="showSidebar = false">
             <FilmIcon class="size-6 text-light-gray" />
-            <span class="ml-3 mt-0.5">Time spans</span>
+            <span class="ml-3 mt-0.5">Seasons</span>
           </router-link>
           <router-link to="/events" class="flex items-center text-light rounded-lg 
         hover:bg-dark-gray p-2" @click.stop="showSidebar = false">
             <CalendarDaysIcon class="size-6 text-light-gray" />
             <span class="ml-3 mt-0.5">Events</span>
+          </router-link>
+          <router-link to="/people" class="flex items-center text-light rounded-lg 
+        hover:bg-dark-gray p-2" @click.stop="showSidebar = false">
+            <UsersIcon class="size-6 text-light-gray" />
+            <span class="ml-3 mt-0.5">People</span>
           </router-link>
           <router-link to="/musics" class="flex items-center text-light rounded-lg 
       hover:bg-dark-gray p-2" @click.stop="showSidebar = false">
@@ -58,7 +63,7 @@
           <hr>
           <router-link to="/account" class="flex items-center text-light rounded-lg 
     hover:bg-dark-gray p-2" @click.stop="showSidebar = false">
-            <UserIcon class="size-6 text-light-gray" />
+            <UserCircleIcon class="size-6 text-light-gray" />
             <span class="ml-3 mt-0.5">Account</span>
           </router-link>
         </nav>
@@ -74,12 +79,13 @@ import { XMarkIcon } from '@heroicons/vue/24/solid'
 import { ListBulletIcon } from '@heroicons/vue/24/solid'
 import { FilmIcon } from '@heroicons/vue/24/solid'
 import { CalendarDaysIcon } from '@heroicons/vue/24/solid'
+import { UsersIcon } from '@heroicons/vue/24/solid'
 import { MusicalNoteIcon } from '@heroicons/vue/24/solid'
-import { UserIcon } from '@heroicons/vue/24/solid'
+import { UserCircleIcon } from '@heroicons/vue/24/solid'
 import { ref } from 'vue'
 import { TransitionRoot, TransitionChild } from '@headlessui/vue'
-import { SITE_NAME } from '@/config';
 import { useRoute, useRouter } from 'vue-router'
+import vClickOutside from '@/directives/clickOutside.js'
 
 const route = useRoute()
 const router = useRouter()
@@ -102,7 +108,7 @@ function triggerSearch() {
     query.search = search.value
     search.value = ''
   }
-  router.push({ path: route.path, query }) 
+  router.push({ path: '/search', query }) 
 }
 
 // Add shortcuts
@@ -113,20 +119,5 @@ window.addEventListener('keydown', (e) => {
     focusSearchBar()
   }
 })
-
-// Directive click outside
-const vClickOutside = {
-  beforeMount(el, binding) {
-    el.clickOutsideEvent = function (event) {
-      if (!(el == event.target || el.contains(event.target))) {
-        binding.value()
-      }
-    }
-    document.body.addEventListener('click', el.clickOutsideEvent)
-  },
-  unmounted(el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent)
-  }
-}
 
 </script>
