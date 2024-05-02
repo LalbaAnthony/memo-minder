81c8fccf9b55e5bbde29cad48bd90f004e07b5e7
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-03 01:44:55 +0200

    todo

--- front/src/pages/SearchPage.vue
+++ front/src/pages/SearchPage.vue
@@ -2,11 +2,17 @@
   <div>
     WIP
     <p> Search: {{ route.query.search }}</p>
+
+    <!-- Also search in (possible ?)  -->
+    <pre>
+      {{ router.options.routes }}
+    </pre>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
+const router = useRouter()
 </script>
