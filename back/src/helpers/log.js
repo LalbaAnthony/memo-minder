a029c88e843debf8a5ac7a2081bbc3144706041e
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-08 16:53:05 +0200

    Fix log.js

--- back/src/helpers/log.js
+++ back/src/helpers/log.js
@@ -1,10 +1,13 @@
 module.exports = (msg) => {
+
+    const LOG_PATH = 'logs/log.log';
+
     const fs = require('fs');
 
     const date = new Date();
     const log = `${date.toISOString()} | ${msg}\n`;
 
-    fs.appendFile('logs/log.txt', log, (err) => {
+    fs.appendFile(LOG_PATH, log, (err) => {
         if (err) {
             console.error(err);
         }
