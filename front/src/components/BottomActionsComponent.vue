7ab7096db227069be5dce7da2493477b7e21474a
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-05-14 13:37:31 +0200

    Imrpove search

--- front/src/components/BottomActionsComponent.vue
+++ front/src/components/BottomActionsComponent.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="fixed bottom-0 right-0 z-30 flex flex-col justify-end gap-2 p-4">
+    <!-- Scroll to top -->
     <div v-if="isMobile()" @click="goToTop()"
       class="flex items-center justify-center bg-dark-gray text-white rounded-full p-4 hover:scale-105 transition-transform duration-200 border border-dark-gray">
       <ChevronUpIcon class="h-6 w-6" />
     </div>
+
+    <!-- Add button -->
     <router-link :to="`${route.path}/add`"
       class="flex items-center justify-center bg-primary text-white rounded-full p-4 hover:scale-105 transition-transform duration-200 border border-dark-gray">
       <PlusIcon class="h-6 w-6" />
     </router-link>
+    
   </div>
 </template>
 
--- front/src/pages/SearchPage.vue
+++ front/src/pages/SearchPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- Search -->
+    <!-- Search bar -->
     <div class="py-2 px-4">
       <form action="" @submit.prevent="loadSearch()">
         <input v-model="search" id="search" type="search" class="w-full px-4 p-2 rounded-lg bg-dark-gray text-light"
@@ -8,9 +8,64 @@
       </form>
     </div>
 
+
+
+
+    <!-- Add buttons -->
+    <div class="py-2 px-4 grid">
+      <TransitionRoot :show="addButtons.season.show">
+        <TransitionChild as="div" enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100" leave="transition ease-in-out duration-300 transform"
+          leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+          <router-link :to="`${'season'}/add`"
+            class="flex justify-center items-center bg-light-dark text-light p-2 rounded-lg cursor-pointer hover:bg-dark-gray">
+            <PlusIcon class="size-6 text-primary mr-3" />
+            <FilmIcon class="size-6 text-light" />
+            <span class="ml-3 mt-0.5">Add as a seaon</span>
+          </router-link>
+        </TransitionChild>
+      </TransitionRoot>
+      <TransitionRoot :show="addButtons.event.show">
+        <TransitionChild as="div" enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100" leave="transition ease-in-out duration-300 transform"
+          leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+          <router-link :to="`${'event'}/add`"
+            class="flex justify-center items-center bg-light-dark text-light p-2 rounded-lg cursor-pointer hover:bg-dark-gray">
+            <PlusIcon class="size-6 text-primary mr-3" />
+            <CalendarDaysIcon class="size-6 text-light" />
+            <span class="ml-3 mt-0.5">Add as an event</span>
+          </router-link>
+        </TransitionChild>
+      </TransitionRoot>
+      <TransitionRoot :show="addButtons.person.show">
+        <TransitionChild as="div" enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100" leave="transition ease-in-out duration-300 transform"
+          leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+          <router-link :to="`${'person'}/add`"
+            class="flex justify-center items-center bg-light-dark text-light p-2 rounded-lg cursor-pointer hover:bg-dark-gray">
+            <PlusIcon class="size-6 text-primary mr-3" />
+            <UsersIcon class="size-6 text-light" />
+            <span class="ml-3 mt-0.5">Add as a person</span>
+          </router-link>
+        </TransitionChild>
+      </TransitionRoot>
+      <TransitionRoot :show="addButtons.music.show">
+        <TransitionChild as="div" enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 scale-95"
+          enter-to="opacity-100 scale-100" leave="transition ease-in-out duration-300 transform"
+          leave-from="opacity-100 scale-100" leave-to="opacity-0 scale-95">
+          <router-link :to="`${'music'}/add`"
+            class="flex justify-center items-center bg-light-dark text-light p-2 rounded-lg cursor-pointer hover:bg-dark-gray">
+            <PlusIcon class="size-6 text-primary mr-3" />
+            <MusicalNoteIcon class="size-6 text-light" />
+            <span class="ml-3 mt-0.5">Add as a music</span>
+          </router-link>
+        </TransitionChild>
+      </TransitionRoot>
+    </div>
+
     <!-- Nb of results -->
     <div v-if="results.length > 0">
-      <p class="text-center text-light-gray text-sm p-4">
+      <p class="text-center text-light-gray text-sm p-4 pt-8">
         {{ results.length }} {{ results.length > 1 ? 'results' : 'result' }}
       </p>
     </div>
@@ -18,15 +73,15 @@
     <!-- Results -->
     <Grid :items="results">
       <template #item="{ item }">
-        <router-link :to="item.path" class="item block p-4 border-b border-gray flex items-center justify-between">
+        <router-link :to="item.path" class="item block p-4 border-b border-gray flex items-center justify-between"
+          :style="['animation-delay: 0.' + randomInt(1, 10) / 15 + 's;']">
           <DocumentTextIcon v-if="item.type === 'page'" class="size-8 text-light-gray" />
           <h3 class="text-lg font-semibold">{{ item.title }}</h3>
           <ArrowLongRightIcon
-            class="arrow size-8 text-light-gray ml-2 hover:text-light transition ease-in-out duration-300 transform" />
+            class="arrow size-8 text-primary ml-2 hover:text-light transition ease-in-out duration-300 transform" />
         </router-link>
       </template>
     </Grid>
-
   </div>
 </template>
 
@@ -34,19 +89,38 @@
 import { onMounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import Grid from '@/components/GridComponent.vue'
+import { PlusIcon } from '@heroicons/vue/24/solid'
 import { DocumentTextIcon } from '@heroicons/vue/24/solid'
 import { ArrowLongRightIcon } from '@heroicons/vue/24/solid'
+import { FilmIcon } from '@heroicons/vue/24/solid'
+import { CalendarDaysIcon } from '@heroicons/vue/24/solid'
+import { UsersIcon } from '@heroicons/vue/24/solid'
+import { MusicalNoteIcon } from '@heroicons/vue/24/solid'
+import { TransitionRoot, TransitionChild } from '@headlessui/vue'
+import { randomInt } from '@/helpers/helpers.js'
 
 const route = useRoute()
 const router = useRouter()
 
 const search = ref(route.query.search)
 const results = ref([])
+const addButtons = ref({
+  season: { show: false },
+  event: { show: false },
+  person: { show: false },
+  music: { show: false }
+})
 
 function loadSearch() {
   if (!search.value) return
 
   results.value = []
+
+  // Show add buttons
+  if (search.value.toLowerCase().includes('season')) addButtons.value.season.show = true
+  if (search.value.toLowerCase().includes('event')) addButtons.value.event.show = true
+  if (search.value.toLowerCase().includes('person')) addButtons.value.person.show = true
+  if (search.value.toLowerCase().includes('music')) addButtons.value.music.show = true
 
   // Pages
   router.options.routes.forEach((route) => {
@@ -86,7 +160,28 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 1rem;
+}
+
 .item:hover .arrow {
   /* transform: translateX(10px); */
+}
+
+.item {
+  animation-name: slide-in;
+  animation-duration: 0.3s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-100%);
+  }
+
+  to {
+    transform: translateX(0);
+  }
 }
 </style>
