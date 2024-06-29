658b226685c78031928c17e1cff65e1f289c339c
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-06-29 13:03:25 +0200

    Push

--- front/src/pages/error/ErrorPage.vue
+++ front/src/pages/error/ErrorPage.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-4xl font-bold text-primary my-8">404</h1>
+  <div class="flex flex-col items-center justify-center gap-4 h-screen">
     <p class="text-2xl font-bold text-light">Page Not Found</p>
     <p class="text-lg text-light-gray">The page you are looking for does not exist.</p>
     <router-link to="/" class="text-light text-lg font-bold underline">Home page</router-link>
