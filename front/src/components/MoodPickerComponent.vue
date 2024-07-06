7f09292855864fbafa9af5dd81fb1bc54a7403d5
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-07-06 20:34:07 +0200

    Damn mood

--- front/src/components/MoodPickerComponent.vue
+++ front/src/components/MoodPickerComponent.vue
@@ -1,58 +1,76 @@
 <template>
-  {{ selected }}
-  <Combobox v-model="selected">
-    <div class="relative w-48 cursor-default">
-      <!-- :displayValue="((mood) => mood.name)" -->
-      <ComboboxInput class="w-48 py-2 pl-3 pr-10 bg-dark-gray text-light rounded-lg"
-        @change="search = $event.target.value" />
-      <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
-        <ChevronUpDownIcon class="size-6 text-light-gray" aria-hidden="true" />
-      </ComboboxButton>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <button
+        class="inline-flex w-full justify-center items-center rounded-full px-3 py-1 text-md font-medium text-light bg-primary"
+        :style="mood ? `background-color: ${mood?.color} !important;` : ''" @click.stop="show = !show">
+        {{ mood?.name || 'Mood' }}
+        <ChevronDownIcon
+          :class="['ml-2 h-5 w-5 text-light transform transition-transform duration-200', show ? 'rotate-180' : '']"
+          aria-hidden="true" />
+      </button>
     </div>
-    <TransitionRoot leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0"
-      @after-leave="search = ''">
-      <ComboboxOptions class="absolute mt-2 max-h-60 w-48 overflow-auto rounded-lg bg-dark-gray text-light">
-        <ComboboxOption v-for="mood in filteredMoods" :key="mood.id" :value="mood" v-slot="{ active }">
-          <li :class="['flex justify-right p-2 cursor-pointer', active ? 'bg-primary' : '']">
+    <TransitionRoot :show="show">
+      <!-- Hide at click outside -->
+      <TransitionChild v-click-outside="() => show = false" as="div" enter="transition ease-out duration-100 transform"
+        enter-from="opacity-0 scale-95" enter-to="opacity-100 scale-100"
+        leave="transition ease-in duration-75 transform" leave-from="opacity-100 scale-100"
+        leave-to="opacity-0 scale-95"
+        class="origin-top-right absolute mt-2 w-48 rounded-lg bg-gray z-10 overflow-auto py-2">
+        <div class="flex flex-col gap-2">
+          <button v-for="mood in moodStore.moods.data" :key="mood.moodId"
+            class="flex items-center justify-between w-full px-2 py-1 text-light hover:bg-light-gray"
+            @click="setMood(mood.moodId)">
             <span>{{ mood.name }}</span>
-          </li>
-        </ComboboxOption>
-      </ComboboxOptions>
+          </button>
+        </div>
+      </TransitionChild>
     </TransitionRoot>
-  </Combobox>
+  </Menu>
 </template>
 
 <script setup>
-import { ref, watch, computed } from 'vue'
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxButton,
-  ComboboxOptions,
-  ComboboxOption,
-  TransitionRoot,
-} from '@headlessui/vue'
-import { ChevronUpDownIcon } from '@heroicons/vue/24/solid'
+import { onMounted, ref, watch } from 'vue'
+import { Menu, TransitionRoot, TransitionChild } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/24/solid'
+import vClickOutside from '@/directives/clickOutside.js'
 import { useMoodStore } from '@/stores/mood'
+
+const props = defineProps({
+  value: {
+    type: Number,
+    required: false,
+  },
+})
 
 const moodStore = useMoodStore()
 
-if (!moodStore.moods.data || moodStore.moods.data.length === 0) moodStore.fetchMoods()
+const show = ref(false)
+const moodId = ref(props.value)
+const mood = ref({})
 
 const emit = defineEmits(['update'])
 
-const search = ref('')
-const selected = ref(moodStore.moods.data[0] || null)
+function setMood(id) {
+  moodId.value = id
+  show.value = false
+  mood.value = moodStore.moods.data.find((mood) => mood.moodId === moodId.value)
+}
 
-const filteredMoods = computed(() =>
-  search.value === ''
-    ? moodStore.moods.data
-    : moodStore.moods.data.filter((mood) =>
-      mood.name.toLowerCase().replace(/\s+/g, '').includes(search.value.toLowerCase().replace(/\s+/g, ''))
-    )
-)
-watch(() => selected.value, () => {
-  emit('update', selected.value.moodId)
+onMounted(() => {
+  if (!moodStore.moods.data || moodStore.moods.data.length === 0) {
+    moodStore.fetchMoods().then(() => {
+      setMood(moodId.value)
+    })
+  }
+  else {
+    setMood(moodId.value)
+  }
+})
+
+
+watch(() => moodId.value, () => {
+  emit('update', moodId.value)
 })
 
 </script>
