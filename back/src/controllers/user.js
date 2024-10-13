7e006b6ee6fdc24a2a2d52c99e57502e3a0db035
LalbaAnthony <anthonylalaba24@gmail.com>
2024-10-13 12:13:50 +0200

    Pushh just in case

--- back/src/controllers/user.js
+++ back/src/controllers/user.js
@@ -177,20 +177,20 @@ exports.validateToken = async (req, res) => {
     const { userId, token } = req.body;
     try {
         // Check if all fields are provided
-        if (!userId) return res.status(400).json({ valid: false, error: 'Missing fields: userId' });
-        if (!token) return res.status(400).json({ valid: false, error: 'Missing fields: token' });
+        if (!userId) return res.status(400).json(formatRes('error', null, 'Missing fields: userId'));
+        if (!token) return res.status(400).json(formatRes('error', null, 'Missing fields: token'));
 
         // Check if the user exists
         const user = await User.findByPk(userId);
-        if (!user) return res.status(404).json({ valid: false, error: 'User not found' });
+        if (!user) return res.status(404).json(formatRes('error', null, 'User not found'));
 
         // Check if the token is valid
         jwt.verify(token, process.env.BACK_SECRET_KEY, (err, user) => {
-            if (err) return res.sendStatus(401).json({ valid: false, error: 'Token invalide' });
+            if (err) return res.sendStatus(403).json(formatRes('error', null, 'Error while verifiying the token'));
         });
 
-        return res.status(201).json({ valid: true, error: 'Token valide' });
+        return res.status(201).json(formatRes('success', null, 'Token is valid'))
     } catch (error) {
-        return res.status(401).json({ valid: false, error: 'Token invalide' });
+        return res.status(500).json(formatRes('error', null, 'Token is invalid'))
     }
 };
