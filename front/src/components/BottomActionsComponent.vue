7e6142a3672a320bfc960b268e41e7d77dfcf74b
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-03-09 23:06:23 +0100

    Misc front fixes

--- front/src/components/BottomActionsComponent.vue
+++ front/src/components/BottomActionsComponent.vue
@@ -40,8 +40,9 @@
       <TransitionRoot :show="showAddSelector">
         <!-- Hide at click outside -->
         <TransitionChild v-click-outside="() => showAddSelector = false" as="div"
-          enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 translate-y-1/4" enter-to="translate-y-0 opacity-100"
-          leave="transition ease-in-out duration-300 transform" leave-from="translate-y-0 opacity-100" leave-to="opacity-0 translate-y-1/4"
+          enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 translate-y-1/4"
+          enter-to="translate-y-0 opacity-100" leave="transition ease-in-out duration-300 transform"
+          leave-from="translate-y-0 opacity-100" leave-to="opacity-0 translate-y-1/4"
           class="origin-top flex flex-col items-center justify-center gap-1 bg-dark-light rounded-t-2xl px-1 py-2 z-10">
           <router-link to="/seasons/add" class="text-primary p-2 rounded-xl hover:bg-gray-dark">
             <FilmIcon class="size-8 text-gray-light" />
