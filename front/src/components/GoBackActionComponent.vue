63ae5fae58005ac26e719e003616ed172aaa094b
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-03-09 21:52:56 +0100

    Misc front fixes

--- front/src/components/GoBackActionComponent.vue
+++ front/src/components/GoBackActionComponent.vue
@@ -1,17 +1,12 @@
 <template>
   <!-- Go back button -->
-  <div
-    v-if="route?.name && (route.name.includes('details') || route.name.includes('add')) || route.name === 'infos' || route.name === 'terms-and-conditions'"
-    @click="goBack()" class="flex items-center justify-start cursor-pointer text-gray-light">
+  <div class="flex items-center justify-start cursor-pointer text-gray-light" @click="goBack()">
     <ChevronLeftIcon class="size-6" />
   </div>
 </template>
 
 <script setup>
 import { ChevronLeftIcon } from '@heroicons/vue/24/outline'
-import { useRoute, } from 'vue-router'
-
-const route = useRoute()
 
 function goBack() {
   window.history.back()
