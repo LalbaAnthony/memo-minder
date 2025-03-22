b46f529152e06d26fa5488f24746c2221f78dd8a
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-03-22 18:02:38 +0100

    Lil changes on front

--- front/src/directives/clickOutside.js
+++ front/src/directives/clickOutside.js
@@ -1,13 +1,15 @@
 export default {
   beforeMount(el, binding) {
     el.clickOutsideEvent = function (event) {
-      if (!(el == event.target || el.contains(event.target))) {
-        binding.value()
+      if (!(el === event.target || el.contains(event.target))) {
+        event.stopPropagation();
+        binding.value();
       }
-    }
-    document.body.addEventListener('click', el.clickOutsideEvent)
+    };
+    // Attach in capture phase (third argument true)
+    document.body.addEventListener('click', el.clickOutsideEvent, true);
   },
   unmounted(el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent)
+    document.body.removeEventListener('click', el.clickOutsideEvent, true);
   }
-}
+};
