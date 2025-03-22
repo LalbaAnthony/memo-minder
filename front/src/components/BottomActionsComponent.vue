27c3f8d9412c186ec91a532a9e9ac93010036a09
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-03-22 19:14:34 +0100

    Lil changes on front

--- front/src/components/BottomActionsComponent.vue
+++ front/src/components/BottomActionsComponent.vue
@@ -1,5 +1,6 @@
 <template>
-  <div :class="['fixed z-20 flex flex-col justify-end gap-2 p-3', isMobile() ? 'bottom-20 right-0' : 'bottom-0 right-0']">
+  <div
+    :class="['fixed z-20 flex flex-col justify-end gap-2 p-3', isMobile() ? 'bottom-20 right-0' : 'bottom-0 right-0']">
     <!-- Scroll to top -->
     <TransitionRoot :show="props.goTopButton && route.name.includes('list') && isMobile() && enableGoTop"
       @click="goTop()">
@@ -31,15 +32,14 @@
 
     <!-- Add button -->
     <div v-if="props.addButton && (route.name === 'home' || route.name.includes('list'))"
-    :class="[showAddSelector ? 'bg-gray-dark' : '', 'rounded-2xl']"
-    >
+      :class="[showAddSelector ? 'bg-gray-dark' : '', 'rounded-2xl']">
       <TransitionRoot :show="showAddSelector">
         <!-- Hide at click outside -->
         <TransitionChild v-click-outside="() => showAddSelector = false" as="div"
           enter="transition ease-in-out duration-300 transform" enter-from="opacity-0 translate-y-1/4"
           enter-to="translate-y-0 opacity-100" leave="transition ease-in-out duration-300 transform"
           leave-from="translate-y-0 opacity-100" leave-to="opacity-0 translate-y-1/4"
-          class="origin-top flex flex-col items-center justify-center gap-1 bg-gray-dark rounded-2xl px-1 py-2 z-10">
+          class="origin-top flex flex-col items-center justify-center gap-1 bg-gray-dark rounded-2xl px-1 py-2">
           <router-link to="/seasons/add" class="p-2 rounded-xl hover:bg-gray">
             <FilmIcon class="size-8 text-light" />
           </router-link>
@@ -124,6 +124,8 @@ function goTop() {
     top: 0,
     behavior: 'smooth',
   })
+
+  enableGoTop.value = false
 }
 
 function triggerCreate() {
@@ -140,7 +142,11 @@ function triggerDelete() {
 
 // Enable go top button when scrolling
 window.addEventListener('scroll', debounce(() => {
-  enableGoTop.value = window.scrollY > 100
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
+  const scrolledOverHalf = scrollPosition > pageHeight * 0.5
+
+  enableGoTop.value = scrolledOverHalf && window.scrollY > 100
 }, 100))
 
 </script>
