901fa818b9c045437214b005c3c2dc1c9360990a
LalbaAnthony <anthonylalaba24@gmail.com>
2025-04-02 00:45:12 +0200

    Misc

--- front/src/components/ProgressBarComponent.vue
+++ front/src/components/ProgressBarComponent.vue
@@ -2,7 +2,7 @@
   <div class="overflow-hidden h-7 my-2 flex rounded-lg bg-gray-dark">
     <div :style="`width: ${props.value}%`"
       :class="['flex flex-col text-left text-gray-dark justify-center pl-3', props.value > 0 ? 'bg-primary' : '']">
-      <div v-if="props.value > 15"> {{ roundNb(props.value, 0) }} %</div>
+      <div v-if="props.value > 15"> {{ roundNb(props.value, 1) }} %</div>
     </div>
   </div>
 </template>
