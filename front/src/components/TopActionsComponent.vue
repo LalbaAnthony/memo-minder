7202a295a1c9a3046fb62a1c7178632d657c122d
Anthony LALBA <a.lalba@agoravita.com>
2025-04-03 14:06:30 +0200

    Misc

--- front/src/components/TopActionsComponent.vue
+++ front/src/components/TopActionsComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-start mt-2">
     <!-- Go back button -->
-    <div v-if="props.goBackButton" class=" cursor-pointer text-gray-light" @click="goBack()">
+    <div v-if="props.goBackButton" class="cursor-pointer text-gray-light" @click="goBack()">
       <ChevronLeftIcon class="size-7" />
     </div>
   </div>
