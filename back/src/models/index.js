3fc8a5e4981c00ada784feaf038fd76356c2f1bd
LalbaAnthony <anthonylalaba24@gmail.com>
2025-04-05 23:36:45 +0200

    rexork

--- back/src/models/index.js
+++ back/src/models/index.js
@@ -1,4 +1,24 @@
-const sequelize = require('../database');
+const { Sequelize } = require('sequelize');
+const logConsole = require('../helpers/logConsole');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
+const sequelizeDevelopment = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: logConsole('Database connection established!', 'info')
+});
+
+const sequelizeProduction = new Sequelize(process.env.BACK_DATABASE_NAME, process.env.BACK_DATABASE_USERNAME, process.env.BACK_DATABASE_PASSWORD, {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306,
+    logging: logConsole('Database connection established!', 'info')
+});
+
+const sequelize = process.env.BACK_ENV === 'production' ? sequelizeProduction : sequelizeDevelopment;
 
 const models = {
   User: require("./userModel")(sequelize),
