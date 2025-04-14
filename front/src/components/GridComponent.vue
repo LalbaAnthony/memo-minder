1619b4b33a04fd37c84a65f4bf5a656d832fcc13
LalbaAnthony <anthonylalaba24@gmail.com>
2025-04-15 01:02:29 +0200

    Decrease grid breakpoint

--- front/src/components/GridComponent.vue
+++ front/src/components/GridComponent.vue
@@ -46,7 +46,7 @@ const props = defineProps({
 <style scoped>
 .grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
   grid-gap: 1rem;
 }
 
