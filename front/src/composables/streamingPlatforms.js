b52ee41757968e5835d42493db9ca96b2cd3acb3
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-04-14 12:13:36 +0200

    Start implement streamin link

---
+++ front/src/composables/streamingPlatforms.js
@@ -0,0 +1,46 @@
+export const streamingPlatforms = {
+    spotify: {
+        colors: {
+            color: '#1DD05D',
+            background: '#080808',
+        },
+        links: {
+            web: 'https://open.spotify.com',
+            app: 'spotify://',
+            search: 'https://open.spotify.com/search/',
+        },
+    },
+    apple: {
+        colors: {
+            color: '#FFFFFF',
+            background: '#FB3D55',
+        },
+        links: {
+            web: 'https://open.spotify.com',
+            app: 'spotify://',
+            search: 'https://open.spotify.com/search/',
+        },
+    },
+    youtube: {
+        colors: {
+            color: '#FFFFFF',
+            background: '#FF0808',
+        },
+        links: {
+            web: 'https://open.spotify.com',
+            app: 'spotify://',
+            search: 'https://open.spotify.com/search/',
+        },
+    },
+    deezer: {
+        colors: {
+            color: '#A238FF',
+            background: '#080808',
+        },
+        links: {
+            web: 'https://open.spotify.com',
+            app: 'spotify://',
+            search: 'https://open.spotify.com/search/',
+        },
+    },
+}
