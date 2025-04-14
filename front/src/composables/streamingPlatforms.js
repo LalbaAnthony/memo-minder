046ee6df6e524c96caac1b2f7b49b387e62779c3
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-04-14 14:34:46 +0200

    Streaming music plateforms array

--- front/src/composables/streamingPlatforms.js
+++ front/src/composables/streamingPlatforms.js
@@ -1,46 +1,54 @@
 export const streamingPlatforms = {
     spotify: {
+        slug: 'spotify',
+        label: 'Spotify',
+        regex: /(spotify)/i,
         colors: {
             color: '#1DD05D',
             background: '#080808',
         },
         links: {
-            web: 'https://open.spotify.com',
-            app: 'spotify://',
+            home: 'https://open.spotify.com',
             search: 'https://open.spotify.com/search/',
         },
     },
     apple: {
+        slug: 'apple',
+        label: 'Apple',
+        regex: /(apple)/i,
         colors: {
             color: '#FFFFFF',
             background: '#FB3D55',
         },
         links: {
-            web: 'https://open.spotify.com',
-            app: 'spotify://',
-            search: 'https://open.spotify.com/search/',
+            home: 'https://music.apple.com/',
+            search: 'https://music.apple.com/search?term=',
         },
     },
     youtube: {
+        slug: 'youtube',
+        label: 'Youtube',
+        regex: /(youtube|youtu.be)/i,
         colors: {
             color: '#FFFFFF',
             background: '#FF0808',
         },
         links: {
-            web: 'https://open.spotify.com',
-            app: 'spotify://',
-            search: 'https://open.spotify.com/search/',
+            home: 'https://www.youtube.com/',
+            search: 'https://www.youtube.com/results?search_query=',
         },
     },
     deezer: {
+        slug: 'deezer',
+        label: 'Deezer',
+        regex: /(deezer)/i,
         colors: {
             color: '#A238FF',
             background: '#080808',
         },
         links: {
-            web: 'https://open.spotify.com',
-            app: 'spotify://',
-            search: 'https://open.spotify.com/search/',
+            home: 'https://www.deezer.com/',
+            search: 'https://www.deezer.com/search/',
         },
     },
 }
