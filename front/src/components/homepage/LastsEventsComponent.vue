03f0450009c9e20eed9c643680799a87e7a06a88
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-05-05 11:40:10 +0200

    Refacto lasts on home page

---
+++ front/src/components/homepage/LastsEventsComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <section>
+            <h2 class="text-xl font-bold">Lasts events added</h2>
+            <div class="my-4">
+                <Loader v-if="eventStore.items.loading" position="static" />
+                <div v-else>
+                    <Grid v-if="eventStore.items?.data?.length > 0" :items="eventStore.items.data">
+                        <template #item="{ item }">
+                            <Event :event="item" />
+                        </template>
+                    </Grid>
+                    <router-link to="/events/add" :class="['mt-6 flex items-center justify-center rounded-lg border-dashed border-2 cursor-pointer border-gray transition-all transition-200',
+                        eventStore.items?.data?.length === 0 ? 'p-16' : 'py-1.5']">
+                        <span v-if="eventStore.items?.data?.length === 0" class="text-light"> Add your first
+                            event</span>
+                        <PlusIcon
+                            class="arrow size-8 text-primary ml-2 transition ease-in-out duration-300 transform" />
+                    </router-link>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup>
+import Grid from '@/components/GridComponent.vue'
+import Event from '@/components/event/EventItem.vue'
+import Loader from '@/components/LoaderComponent.vue'
+import { PlusIcon } from '@heroicons/vue/24/solid'
+import { useAuthStore } from '@/stores/auth'
+import { useEventStore } from '@/stores/event'
+import { onMounted } from 'vue'
+
+const authStore = useAuthStore()
+const eventStore = useEventStore()
+
+onMounted(() => {
+    if (authStore?.user?.homePageEnableLasts) {
+        eventStore.fetchItems({
+            sort: [{ order: 'DESC', orderBy: 'createdAt' }],
+            perPage: 3
+        })
+    }
+})
+
+</script>
--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -26,44 +26,8 @@
     </div>
 
     <div class="md:grid md:grid-cols-2 md:gap-24">
-      <section v-if="authStore?.user?.homePageEnableLasts">
-        <h2 class="text-xl font-bold">Lasts events added</h2>
-        <div class="my-4">
-          <Loader v-if="eventStore.items.loading" position="static" />
-          <div v-else>
-            <Grid v-if="eventStore.items?.data?.length > 0" :items="eventStore.items.data">
-              <template #item="{ item }">
-                <Event :event="item" />
-              </template>
-            </Grid>
-            <router-link to="/events/add" :class="['mt-6 flex items-center justify-center rounded-lg border-dashed border-2 cursor-pointer border-gray transition-all transition-200',
-              eventStore.items?.data?.length === 0 ? 'p-16' : 'py-1.5']">
-              <span v-if="eventStore.items?.data?.length === 0" class="text-light"> Add your first
-                event</span>
-              <PlusIcon class="arrow size-8 text-primary ml-2 transition ease-in-out duration-300 transform" />
-            </router-link>
-          </div>
-        </div>
-      </section>
-      <section v-if="authStore?.user?.homePageEnableLasts">
-        <h2 class="text-xl font-bold">Lasts seasons added</h2>
-        <div class="my-4">
-          <Loader v-if="seasonStore.items.loading" position="static" />
-          <div v-else>
-            <Grid v-if="seasonStore.items?.data?.length > 0" :items="seasonStore.items.data">
-              <template #item="{ item }">
-                <Season :season="item" />
-              </template>
-            </Grid>
-            <router-link to="/seasons/add" :class="['mt-6 flex items-center justify-center rounded-lg border-dashed border-2 cursor-pointer border-gray transition-all transition-200',
-              seasonStore.items?.data?.length === 0 ? 'p-16' : 'py-1.5']">
-              <span v-if="seasonStore.items?.data?.length === 0" class="text-light"> Add your first
-                season</span>
-              <PlusIcon class="arrow size-8 text-primary ml-2 transition ease-in-out duration-300 transform" />
-            </router-link>
-          </div>
-        </div>
-      </section>
+      <LastsEventsComponent v-if="authStore?.user?.homePageEnableLasts" />
+      <LastsSeasonsComponent v-if="authStore?.user?.homePageEnableLasts" />
     </div>
 
     <Footer />
@@ -73,39 +37,20 @@
 </template>
 
 <script setup>
-import Grid from '@/components/GridComponent.vue'
 import Footer from '@/components/FooterComponent.vue'
-import Season from '@/components/season/SeasonItem.vue'
-import Event from '@/components/event/EventItem.vue'
 import Stats from '@/components/homepage/StatsComponent.vue'
 import Spents from '@/components/homepage/SpentsComponent.vue'
+import LastsEventsComponent from '@/components/homepage/LastsEventsComponent.vue'
+import LastsSeasonsComponent from '@/components/homepage/LastsSeasonsComponent.vue'
 import BottomActions from '@/components/actions/BottomActionsComponent.vue'
-import Loader from '@/components/LoaderComponent.vue'
-import { PlusIcon } from '@heroicons/vue/24/solid'
 import { useAuthStore } from '@/stores/auth'
-import { useSeasonStore } from '@/stores/season'
-import { useEventStore } from '@/stores/event'
 import { useQuoteStore } from '@/stores/quote'
 import { onMounted } from 'vue'
 
 const authStore = useAuthStore()
 const quoteStore = useQuoteStore()
-const seasonStore = useSeasonStore()
-const eventStore = useEventStore()
 
-// Fetch data on mount
 onMounted(() => {
-  if (authStore?.user?.homePageEnableLasts) {
-    eventStore.fetchItems({
-      sort: [{ order: 'DESC', orderBy: 'createdAt' }],
-      perPage: 3
-    })
-    seasonStore.fetchItems({
-      sort: [{ order: 'DESC', orderBy: 'createdAt' }],
-      perPage: 3
-    })
-  }
-
   if (authStore?.user?.homePageEnableQuote) quoteStore.fetchQuoteIfTooOld()
 })
 
