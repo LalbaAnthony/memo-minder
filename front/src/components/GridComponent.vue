22e39f802be1be0d8cfe707e8d8f0462cb4973ac
Anthony LALBA <a.lalba@agoravita.com>
2025-05-20 17:09:42 +0200

    Improve grid

--- front/src/components/GridComponent.vue
+++ front/src/components/GridComponent.vue
@@ -46,7 +46,7 @@ const props = defineProps({
 <style scoped>
 .grid {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(min(350px, 100%), 1fr));
   grid-gap: 1rem;
 }
 
