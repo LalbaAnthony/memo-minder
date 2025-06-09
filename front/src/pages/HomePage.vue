e4661890d005bd54bc1eb680305160b3cbef6d51
LalbaAnthony <anthonylalaba24@gmail.com>
2025-06-09 12:04:13 +0200

    Misc on front

--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
-    <Spents v-if="authStore?.user?.homePageEnableSpents" :birthdate="authStore.user.birthdate" />
-
-    <div class="md:grid md:grid-cols-2 md:gap-6">
-      <Stats v-if="authStore?.user?.homePageEnableStats && authStore?.user?.birthdate" :birthdate="authStore.user.birthdate" />
-      <Quote v-if="authStore?.user?.homePageEnableQuote" />
-    </div>
-
-    <div class="md:grid md:grid-cols-2 md:gap-24">
-      <!-- TODO: Disabled because of cache issues on production -->
-      <UpcomingBirthdays v-if="false && authStore?.user?.homePageEnableUpcomings" />
-    </div>
-
-    <div class="md:grid md:grid-cols-2 md:gap-24">
-      <LastsEvents v-if="authStore?.user?.homePageEnableLasts" />
-      <LastsSeasons v-if="authStore?.user?.homePageEnableLasts" />
+    <div class="home">
+      <Spents v-if="authStore?.user?.homePageEnableSpents" :birthdate="authStore.user.birthdate" class="a" />
+      <Stats v-if="authStore?.user?.homePageEnableStats && authStore?.user?.birthdate"
+        :birthdate="authStore.user.birthdate" class="b" />
+      <Quote v-if="authStore?.user?.homePageEnableQuote" class="c" />
+      <UpcomingBirthdays v-if="false && authStore?.user?.homePageEnableUpcomings" class="d" />
+      <LastsEvents v-if="authStore?.user?.homePageEnableLasts" class="e" />
+      <LastsSeasons v-if="authStore?.user?.homePageEnableLasts" class="f" />
     </div>
 
     <Footer />
@@ -36,5 +29,108 @@ import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
 
-
 </script>
+
+<style scoped>
+.home {
+  display: grid;
+  grid-template-rows: auto;
+}
+
+.a {
+  grid-area: a;
+}
+
+.b {
+  grid-area: b;
+}
+
+.c {
+  grid-area: c;
+}
+
+.d {
+  grid-area: d;
+}
+
+.e {
+  grid-area: e;
+}
+
+.f {
+  grid-area: f;
+}
+
+@media (min-width: 1536px) {
+  .home {
+    grid-template-areas:
+      "a a a a a a"
+      "b b c c . ."
+      "d d e e f f";
+    grid-template-columns: repeat(6, 1fr);
+    gap: 3rem 3rem;
+  }
+}
+
+@media (min-width: 1280px) and (max-width: 1535px) {
+  .home {
+    grid-template-areas:
+      "a a a a a a"
+      "b b c c c ."
+      "d d e e f f";
+    grid-template-columns: repeat(6, 1fr);
+    gap: 1rem 3rem;
+  }
+}
+
+@media (min-width: 1024px) and (max-width: 1279px) {
+  .home {
+    grid-template-areas:
+      "a a a a a a"
+      "b b c c c c"
+      "d d d d . ."
+      "e e e f f f";
+    grid-template-columns: repeat(6, 1fr);
+    gap: 1rem 3rem;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  .home {
+    grid-template-areas:
+      "a a a a a a"
+      "b b b c c c"
+      "d d d d . ."
+      "e e e f f f";
+    grid-template-columns: repeat(6, 1fr);
+    gap: 1rem 3rem;
+  }
+}
+
+@media (min-width: 640px) and (max-width: 767px) {
+  .home {
+    grid-template-areas:
+      "a a"
+      "b c"
+      "d d"
+      "e f";
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+  }
+}
+
+@media (max-width: 639px) {
+  .home {
+    grid-template-areas:
+      "a"
+      "b"
+      "c"
+      "d"
+      "e"
+      "f";
+    grid-template-columns: repeat(1, 1fr);
+    gap: 1rem;
+
+  }
+}
+</style>
