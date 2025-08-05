3cf1938e5e7adc27f1b62d0a30cb7aa812fc7b9d
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-08-05 23:10:15 +0200

    Add sw.js

--- front/src/main.js
+++ front/src/main.js
@@ -22,3 +22,15 @@ app.use(router)
 app.use(Toast)
 
 app.mount('#app')
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(reg => {
+                console.log('SW registered, scope:', reg.scope);
+            })
+            .catch(err => {
+                console.error('SW registration failed:', err);
+            });
+    });
+}
