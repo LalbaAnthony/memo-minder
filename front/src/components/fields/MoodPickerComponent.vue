1917c3abdac2db116e0983ab1aa240decc485ba7
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-08-05 22:18:24 +0200

    Add color on mood + Uncomment Up Bdays

--- front/src/components/fields/MoodPickerComponent.vue
+++ front/src/components/fields/MoodPickerComponent.vue
@@ -1,14 +1,18 @@
 <template>
-  <select v-model="moodId" id="mood" class="select sm:w-48 w-full px-4 p-2 rounded-lg bg-gray-dark text-light">
-    <option value="" disabled>Mood</option>
-    <option v-for="(value, key) in moodStore.items.data" :key="key" :value="value.moodId"
-      :selected="value.moodId === moodId">
-      {{ value?.name }}</option>
-  </select>
+  <div class="select sm:w-48 w-full rounded-lg bg-gray-dark text-light flex items-center justify-start gap-2 px-4">
+    <div :style="`background-color: ${mood?.color}`" v-if="mood?.color" class="w-6 h-5 py-2 rounded-full">
+    </div>
+    <select v-model="moodId" id="mood" class="w-full">
+      <option value="" disabled>Mood</option>
+      <option v-for="(value, key) in moodStore.items.data" :key="key" :value="value.moodId"
+        :selected="value.moodId === moodId">
+        {{ value?.name }}</option>
+    </select>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import { useMoodStore } from '@/stores/mood'
 
 const props = defineProps({
@@ -23,6 +27,10 @@ const moodStore = useMoodStore()
 const moodId = ref(props.value)
 
 const emit = defineEmits(['update'])
+
+const mood = computed(() => {
+  return moodStore.items.data.find(item => item.moodId === moodId.value)
+})
 
 onMounted(() => {
   if (!moodStore.items.data || moodStore.items.data.length === 0) moodStore.fetchItems()
